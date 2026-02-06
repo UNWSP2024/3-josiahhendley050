bf40f9c97d52eaeca503d29ed2ac77f505be62d5
josiahhendley050 <josiahhendley050@gmail.com>
2026-02-05 20:40:07 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -8,6 +8,9 @@
 # Over 6 pounds but not more than 10 pounds	$4.00
 # Over 10 pounds	                        $4.75
 # Write a program which calculates the shipping charge and displays the total.
+#Josiah Hendley
+#2/5/26
+#Shipping charges
 
 def weight_conversion(weight):
     # Calculate the shipping charge.
