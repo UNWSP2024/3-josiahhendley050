bb507bea4c3b975646b9f793f5256c453265a208
josiahhendley050 <josiahhendley050@gmail.com>
2026-02-05 20:33:36 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -12,9 +12,15 @@
 def weight_conversion(weight):
     # Calculate the shipping charge.
     shippingCost = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+    if weight <= 2:
+        shippingCost = weight * 1.50
+    elif weight <= 6:
+        shippingCost = weight * 3.00
+    elif weight <= 10:
+        shippingCost = weight * 4.00
+    else:
+        shippingCost = weight * 4.75
+
     
     return shippingCost
 
